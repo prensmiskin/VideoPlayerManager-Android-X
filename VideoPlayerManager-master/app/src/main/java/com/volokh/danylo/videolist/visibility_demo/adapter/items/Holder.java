eabf9334d2c86354658cf12c3e597c06cf903ffd
prensmiskin <oguzgps@gmail.com>
2020-04-30 21:43:31 +0300

    deneme meydey

---
+++ VideoPlayerManager-master/app/src/main/java/com/volokh/danylo/videolist/visibility_demo/adapter/items/Holder.java
@@ -0,0 +1,22 @@
+package com.volokh.danylo.videolist.visibility_demo.adapter.items;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.volokh.danylo.videolist.R;
+
+/**
+ * Created by danylo.volokh on 09.01.2016.
+ */
+public class Holder extends RecyclerView.ViewHolder{
+
+    public final TextView positionView;
+
+    public Holder(View itemView) {
+        super(itemView);
+        positionView = (TextView) itemView.findViewById(R.id.position);
+    }
+}
